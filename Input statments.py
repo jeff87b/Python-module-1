809a7b60e0b58605bec379e2409904aafba3d4ea
jeff87b <jeff87b@gmail.com>
2017-11-01 20:51:34 +0100

    Example of using variables.

--- Input statments.py
+++ Input statments.py
@@ -1,9 +1,13 @@
 print ("Please enter the radius of the circle :")
 
-radius=input()
+radius=input
 
 print("You have entered a radius", radius)
 
+if (radius>0):
+
+
+
 myConstPi = 3.14
 
-print("The circumference of the circle is :", 2*myConstPi*radius")
+print("The circumference of the circle is :", 2*myConstPi*radius)
