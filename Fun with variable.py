2e3f5ea34b9a5a06ce81949f05de719df754c9a6
jeff87b <jeff87b@gmail.com>
2017-11-01 20:34:11 +0100

    Example of using variables.

--- Fun with variable.py
+++ Fun with variable.py
@@ -1,6 +1,9 @@
 #This is variable
 radius = 5
+
 #This is a constant.
-Myconstpy=3.14
+myConstPy=3.14
+
 print("The radius of the cirkle is",radius)
-print("The circumference of the cirle is:", 2*myconstpy)
+
+print("The circumference of the cirle is:", 2*myConstPy*radius)
---
+++ Input statments.py
@@ -0,0 +1,9 @@
+print ("Please enter the radius of the circle :")
+
+radius=input()
+
+print("You have entered a radius", radius)
+
+myConstPi = 3.14
+
+print("The circumference of the circle is :", 2*myConstPi*radius")
