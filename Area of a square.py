87965ab0ac48051a381ca5c9a06fd3bb6c0fc790
jeff87b <33292848+jeff87b@users.noreply.github.com>
2017-11-06 10:32:43 +0100

    Create Area of a square.py

---
+++ Area of a square.py
@@ -0,0 +1,7 @@
+print("This program calculates the area of a square.")
+
+side1=int(input("Enter side number one: "))
+
+side2=int(input("Enter side number two: "))
+
+print("The area is: ", side1*side2)
