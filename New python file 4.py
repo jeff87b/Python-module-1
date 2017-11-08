c6041d886a2088e2ba5cf804e58a626fab3c9553
jeff87b <jeff87b@gmail.com>
2017-11-08 21:02:37 +0100

    4

---
+++ New python file 4.py
@@ -0,0 +1,29 @@
+import random
+
+minimum = int(input("Enter the lower range"))
+maximum = int(input("Enter å higher range"))
+maxTries = 5
+
+myRandomNumber = (random.randint(minimum, maximum))
+theNumberGuessed = int(input("Guess a number betwin 1 and 6"))
+#print(myRandomNumber)
+tries = 0
+
+while(tries < maxTries):
+    if(theNumberGuessed != myRandomNumber):
+        print("Incorrct : Try again!")
+        tries = tries + 1
+        theNumberGuessed = int(input("Guess again"))
+        if(theNumberGuessed == myRandomNumber):
+            print("Wohoo!!")
+            exit(0)
+
+
+print("Unfortunately all the guesses were Wrong")
+
+#What if we enter numbers outside the range
+#Make a way to continue playing the game
+
+print(type(theNumberGuessed))
+if theNumberGuessed==myRandomNumber:
+    print("Woohoo!")
