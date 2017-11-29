b56541d53ebd26f46b412d3ae1e6fde81bb7b102
jeff87b <jeff87b@gmail.com>
2017-11-29 20:37:12 +0100

    Wrote a couple of functions to calculate temperature

--- 022.py
+++ 022.py
@@ -1,15 +1,25 @@
-#defining a function
 
+
+#defining a function
 def celciusToFahrenheit():
-    myInput = int(input("Enter a temperature in celcius: "))
-    tempInFahrenheit = (myInput * 9 + 160) / 5
-    print("The temp of","in Fahrenheit")
+    myInput = int(input("Enter a temperature in Celcius : "))
+    tempInFahrenheit = (myInput*9 + 160) / 5
+    print("The temp of " , myInput , "in celcius is " ,
+          tempInFahrenheit , "in Fahreiheit")
+
 
 def fahrenheitToCelcius():
-    myInput = int(input("Enter a temperature in Fahrenheit: "))
-    tempInCelcius = ((myInput - 32) / 9) *5
-    print("The temp of ", myInput, "in Fahrenheit is")
-        tempInCelcius,"in celcius"
+    myInput = int(input("Enter a temperature in Fahrenheit : "))
+    tempInCelcius = ((myInput - 32)/9)*5
+    print("The temp of " , myInput , "in fahrenheit is" ,
+          tempInCelcius , "in Celcius")
 
+optionSelected = input("Enter 'c' for output in celcius and"
+                       "Enter 'f' for output in fahrenheit")
 #calling a function
-celciusToFahrenheit()
+if(optionSelected == 'c'):
+    fahrenheitToCelcius()
+elif(optionSelected == 'f'):
+    celciusToFahrenheit()
+else:
+    print("You selected an invalid option")
