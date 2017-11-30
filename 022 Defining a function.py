81bc0903f5e1d072a3b385f53029ba7bb5d1db5e
jeff87b <jeff87b@gmail.com>
2017-11-30 21:56:57 +0100

    Wrote a couple of functions to calculate temperature

--- 022 Defining a function.py
+++ 022 Defining a function.py
@@ -1,18 +1,15 @@
-
-
 #defining a function
 def celciusToFahrenheit():
     myInput = int(input("Enter a temperature in Celcius : "))
     tempInFahrenheit = (myInput*9 + 160) / 5
-    print("The temp of " , myInput , "in celcius is " ,
-          tempInFahrenheit , "in Fahreiheit")
+    print("The temp of " , myInput , "in celcius is " , tempInFahrenheit , "in Fahreiheit")
 
 
 def fahrenheitToCelcius():
     myInput = int(input("Enter a temperature in Fahrenheit : "))
     tempInCelcius = ((myInput - 32)/9)*5
-    print("The temp of " , myInput , "in fahrenheit is" ,
-          tempInCelcius , "in Celcius")
+    print("The temp of " , myInput , "in fahrenheit is" , tempInCelcius , "in Celcius")
+    exit(0)
 
 optionSelected = input("Enter 'c' for output in celcius and"
                        "Enter 'f' for output in fahrenheit")
