5e8c9721952b0b9a588c23e5da301230da705fc5
jeff87b <jeff87b@gmail.com>
2017-12-08 20:22:20 +0100

    Homework after day 4.

---
+++ 030 Homework Lists.py
@@ -0,0 +1,53 @@
+#Homework after day 4
+#1) Write a Python function to sum all the items in a list.
+#2) Write a Python function to multiply all the items in a list.
+#3) Write a Python function to get the largest number from a list.
+#4) Write a Python function to print every third number from a list of numbers until the list becomes empty.
+#5) Write a Python function to get the third side of right angled triangle from two given sides. (Pythagorous theorem anyone?)
+import math
+myList = [1, 2, 3, 57, 8, 13, 25, 50, 30]
+
+def addMyList():
+    sumOfList = 0
+    print("Summing up my list of numbers")
+    for i in range(0, len(myList)):
+        sumOfList = sumOfList + myList[i]
+    print("The sum of myList is", sumOfList)
+
+addMyList()
+
+def multiplyMyList():
+    MultiplySum = 1
+    print("The sum of all the items multiplied")
+    for abc in range(0, len(myList)):
+        MultiplySum =  MultiplySum * myList[abc]
+    print("The sum =", MultiplySum)
+
+multiplyMyList()
+
+def highestNumberInList():
+    highestNumber = max(myList)
+    print("Highest number =", highestNumber)
+
+highestNumberInList()
+
+#alternative 1
+def printE3N():
+    print (myList[2], myList[5], myList[8])
+
+printE3N()
+
+#alternative 2
+def printE3N2():
+    for j in range(2, len(myList), 3):
+        print (myList[j])
+
+printE3N2()
+
+
+def hypotenuse():
+    a = 50
+    b = 50
+    print(math.sqrt(a**2 + b**2))
+
+hypotenuse()
