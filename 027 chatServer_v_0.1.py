6353bb7d115db5628dce904ff6bdd27b539651e5
jeff87b <jeff87b@gmail.com>
2017-12-09 18:19:47 +0100

    Choice of task for the course

--- 027 chatServer_v_0.1.py
+++ 027 chatServer_v_0.1.py
@@ -9,15 +9,14 @@ s.bind((host, port))        # Bind to the port
 
 s.listen(5)                 # Now wait for client connection.
 
-while True:
-    c, addr = s.accept()     # Establish connection with client.
-    print 'Got connection from', addr
-    c.send('Successfully established connection to server')
+c, addr = s.accept()     # Establish connection with client.
+print 'Got connection from', addr
+c.send('Successfully established connection to server')
 
-    nickname = raw_input("Enter your nickname: ")
-    while True:
-        print c.recv(1024)
-        message = raw_input(nickname + "-> ")
-        c.send(nickname + ": " + message)
+nickname = raw_input("Enter your nickname: ")
+while True:
+    print c.recv(1024)
+    message = raw_input(nickname + "-> ")
+    c.send(nickname + ": " + message)
 
 #c.close()                # Close the connection
