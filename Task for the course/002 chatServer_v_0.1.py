ea0dcb57e2d0d0b04c6f33120165d770215b48c3
jeff87b <jeff87b@gmail.com>
2017-12-16 21:47:13 +0100

    Choice of task for the course

---
+++ Task for the course/002 chatServer_v_0.1.py
@@ -0,0 +1,23 @@
+# This is chatServer_v_0.1.py file
+
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = '127.0.0.1'          # Lan ip or localhost (for listening)
+port = 33555                # Reserve a port for your service (for listening).
+s.bind((host, port))        # Bind to the port
+
+s.listen(5)                 # Now wait for client connection.
+
+c, addr = s.accept()        # Establish connection with client.
+
+print 'Got connection from', addr
+c.send('Successfully established connection to server')
+
+nickname = raw_input("Enter your nickname: ")
+while True:
+    print c.recv(1024)
+    message = raw_input(nickname + "-> ")
+    c.send(nickname + ": " + message)
+
+#c.close()                # Close the connection
