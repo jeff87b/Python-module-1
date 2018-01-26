1034a497e6ad8f1bc450c97c19eff14efe1bac1d
jeff87b <jeff87b@gmail.com>
2018-01-26 14:47:15 +0100

    Homework after day 8

---
+++ Day 6-10/Day 8/04 Calculate leap year (working).py
@@ -0,0 +1,24 @@
+def isLeapYear(year_entered):
+    check_divisible_by_4 = year_entered % 4
+    check_divisible_by_100 = year_entered % 100
+    if check_divisible_by_4 == 0 and check_divisible_by_100 != 0:
+        print("Yes it's a leap year")
+
+    elif check_divisible_by_4 == 0 and check_divisible_by_100 == 0:
+        check_divisible_by_400 = year_entered % 400
+        if check_divisible_by_100 == 0 and check_divisible_by_400 != 0:
+            print("No, it's not a leap year")
+
+        else:
+            print("Yes it's a leap year")
+
+    else:
+        print("No, it's not a leap year")
+
+    return
+
+
+print("Enter years below")
+while True:
+    yearEntered = int(input())
+    isLeapYear(yearEntered)
