ec7c3d7d85a8627ecbdab7eac2126f3535edd0e9
jeff87b <jeff87b@gmail.com>
2018-01-30 15:28:43 +0100

    Homework after day 8

--- Day 6-10/Day 8/06 Homework, list, season for selected month.py
+++ Day 6-10/Day 8/06 Homework, list, season for selected month.py
@@ -1,6 +1,5 @@
 seasonList = [["January", "February", "December"], ["March", "April", "May"], ["June", "July", "August"], ["September", "October", "November"]]
 
-
 chooseMonth = input("Enter month: ")
 for months in seasonList[0]:
     if months == chooseMonth:
@@ -14,4 +13,3 @@ for months in seasonList[2]:
 for months in seasonList[3]:
     if months == chooseMonth:
         print("Autumn")
-
